00457e70f1cfd8f74f86b0e9bd179a405c4d9117
Gleibson dos Santos Pereira <gleibson.santos05@gmail.com>
2025-06-24 14:49:22 -0300

    proximo comit

--- java/script.js
+++ java/script.js
@@ -1,1 +1,11 @@
+let searchForm = document.querySelector('.header .flex .search-form');
 
+let searchToggler = document.querySelector('#search-toggler');
+
+searchToggler.onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+}
